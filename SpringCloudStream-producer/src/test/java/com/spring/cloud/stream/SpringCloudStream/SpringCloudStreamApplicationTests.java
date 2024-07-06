fc431269158a3a7567396ab7fdd50028a1c50446
sanan <sanan3946@gmail.com>
2024-07-07 04:09:27 +0530

    project init

---
+++ SpringCloudStream-producer/src/test/java/com/spring/cloud/stream/SpringCloudStream/SpringCloudStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.cloud.stream.SpringCloudStream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudStreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
